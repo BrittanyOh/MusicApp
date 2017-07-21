048d9f7b8dad4b441e467716ec52ee268a10ed30
Brittany Ohalete <brittanyohalete96@gmail.com>
2017-07-21 15:10:14 -0400

    UI Changes, Working on Search Function

--- MusicApp/ViewController.swift
+++ MusicApp/ViewController.swift
@@ -11,7 +11,6 @@ class ViewController: UIViewController{
     
 
     @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
-    @IBOutlet weak var menuShadow: NSLayoutConstraint!
     
     var menuShowing = true
     
@@ -23,13 +22,11 @@ class ViewController: UIViewController{
         
         if(menuShowing){
             leadingConstraint.constant = 0
-            menuShadow.constant = -149
             
             UIView.animate(withDuration: 0.3, animations: {self.view.layoutIfNeeded()})
             menuShowing = false
         }else{
             leadingConstraint.constant = -150
-            menuShadow.constant = -158
             UIView.animate(withDuration: 0.2, animations: {self.view.layoutIfNeeded()})
             menuShowing = true
         }
