54dd59d02054971b56a958072cbe84e6bbd46460
AngelCharae <angel_mitchell@comcast.com>
2017-07-21 15:01:32 -0400

    Slider and back button active

--- MusicApp/PlayerVC.swift
+++ MusicApp/PlayerVC.swift
@@ -16,23 +16,61 @@ class PlayerVC : UIViewController {
     @IBOutlet weak var progressView: UIProgressView!
     @IBOutlet weak var songTitleLabel: UILabel!
     @IBOutlet weak var artistLabel: UILabel!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var fowardButton: UIButton!
+    @IBOutlet weak var rewindButton: UIButton!
+    @IBOutlet weak var scrubber: UISlider!
     
     var audioPlayer: AVAudioPlayer = AVAudioPlayer()
     
     @IBAction func playAction(sender: UIButton){
+        if (playButton.image(for: .normal) == #imageLiteral(resourceName: "pause")) {
+            playButton.setImage(#imageLiteral(resourceName: "play"), for: .normal)
+            audioPlayer.pause()
+        } else {
+            playButton.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
+            audioPlayer.prepareToPlay()
+            scrubber.maximumValue = Float(audioPlayer.duration)
+            scrubber.value = 0.0
+            Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(PlayerVC.updateScrubber), userInfo: nil, repeats: true)
+            audioPlayer.play()
+        }
+    }
+    
+    @IBAction func rewindAction(sender: UIButton){
+        if (audioPlayer.isPlaying) {
+            audioPlayer.currentTime = 0
+            audioPlayer.play()
+        } else {
+            audioPlayer.currentTime = 0
+        }
+    }
+    
+    @IBAction func audioTimeSlider(_ slider: UISlider) {
+        audioPlayer.stop()
+        audioPlayer.currentTime = TimeInterval(scrubber.value)
+        audioPlayer.prepareToPlay()
         audioPlayer.play()
     }
     
-    @IBAction func pauseAction(sender: UIButton){
-        
+    func updateScrubber () {
+        scrubber.value = Float(audioPlayer.currentTime)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        //send choosen song to path
+    
         do {
             let audioPath = Bundle.main.path(forResource: "song", ofType: "mp3")
             try audioPlayer = AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: audioPath!) as URL)
+            
+            if (audioPlayer.isPlaying) {
+                playButton.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
+                scrubber.maximumValue = Float(audioPlayer.duration)
+                 Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(PlayerVC.updateScrubber), userInfo: nil, repeats: true)
+            }
+            
         } catch {
             //ERROR
         }
